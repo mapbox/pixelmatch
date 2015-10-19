4e7908f4da8de7f12daa17fd8ec7acf203f741f4
Vladimir Agafonkin <agafonkin@gmail.com>
2015-10-19 13:35:13 +0300

    comment the code thoroughly

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ and [perceptual color metrics](https://en.wikipedia.org/wiki/YUV).
 
 Inspired by [Resemble.js](https://github.com/Huddle/Resemble.js)
 and [Blink-diff](https://github.com/yahoo/blink-diff).
-Unlike these libraries, pixelmatch is around **120 lines of code**,
+Unlike these libraries, pixelmatch is around **90 lines of code**,
 has **no dependencies**, and works on **raw arrays** of image data,
 so it's **blazing fast** and can be used in **any environment** (Node or browsers).
 
--- index.js
+++ index.js
@@ -4,34 +4,49 @@ module.exports = pixelmatch;
 
 function pixelmatch(img1, img2, output, width, height, threshold, includeAA) {
 
-    var maxDelta = 255 * 255 * 3 * (threshold === undefined ? 0.005 : threshold),
+    if (threshold === undefined) threshold = 0.005;
+
+    // scale the difference threshold to the maximum square YUV distance between two colors
+    var maxDelta = 255 * 255 * 3 * threshold,
         diff = 0;
 
+    // compare each pixel of one image against the other one
     for (var y = 0; y < height; y++) {
         for (var x = 0; x < width; x++) {
 
             var pos = (y * width + x) * 4;
+
+            // squared YUV distance between colors at this pixel position
             var delta = colorDelta(img1, img2, pos, pos);
 
+            // the color difference is above the threshold
             if (delta > maxDelta) {
+                // check it's a real rendering difference or just anti-aliasing
                 if (!includeAA && (antialiased(img1, x, y, width, height, img2) ||
                                    antialiased(img2, x, y, width, height, img1))) {
+                    // one of the pixels is anti-aliasing; draw as yellow and do not count as difference
                     drawPixel(output, pos, 255, 255, 0);
 
                 } else {
+                    // found substantial difference not caused by anti-aliasing; draw it as red
                     drawPixel(output, pos, 255, 0, 0);
                     diff++;
                 }
 
             } else {
+                // pixels are similar; draw background as grayscale image blended with white
                 var val = 255 - 0.1 * (255 - grayPixel(img1, pos)) * img1[pos + 3] / 255;
                 drawPixel(output, pos, val, val, val);
             }
         }
     }
 
+    // return the number of different pixels
     return diff;
 }
+
+// check if a pixel is likely a part of anti-aliasing;
+// based on "Anti-aliased Pixel and Intensity Slope Detector" paper by V. Vysniauskas, 2009
 
 function antialiased(img, x1, y1, width, height, img2) {
     var x0 = Math.max(x1 - 1, 0),
@@ -46,25 +61,31 @@ function antialiased(img, x1, y1, width, height, img2) {
         max = 0,
         minX, minY, maxX, maxY;
 
+    // go through 8 adjacent pixels
     for (var x = x0; x <= x2; x++) {
         for (var y = y0; y <= y2; y++) {
             if (x === x1 && y === y1) continue;
 
+            // brightness delta between the center pixel and adjacent one
             var delta = colorDelta(img, img, pos, (y * width + x) * 4, true);
 
+            // count the number of equal, darker and brighter adjacent pixels
             if (delta === 0) zeroes++;
+            else if (delta < 0) negatives++;
+            else if (delta > 0) positives++;
+
+            // if found more than 2 equal siblings, it's definitely not anti-aliasing
             if (zeroes > 2) return false;
 
             if (!img2) continue;
 
-            if (delta < 0) negatives++;
-            else if (delta > 0) positives++;
-
+            // remember the darkest pixel
             if (delta < min) {
                 min = delta;
                 minX = x;
                 minY = y;
             }
+            // remember the brightest pixel
             if (delta > max) {
                 max = delta;
                 maxX = x;
@@ -75,11 +96,17 @@ function antialiased(img, x1, y1, width, height, img2) {
 
     if (!img2) return true;
 
+    // if there are no both darker and brighter pixels among siblings, it's not anti-aliasing
     if (negatives === 0 || positives === 0) return false;
 
+    // if either the darkest or the brightest pixel has more than 2 equal siblings in both images
+    // (definitely not anti-aliased), this pixel is anti-aliased
     return (!antialiased(img, minX, minY, width, height) && !antialiased(img2, minX, minY, width, height)) ||
            (!antialiased(img, maxX, maxY, width, height) && !antialiased(img2, maxX, maxY, width, height));
 }
+
+// calculate either the squared YUV distance between colors,
+// or just the brightness differene (Y component) if yOnly is true
 
 function colorDelta(img1, img2, i, j, yOnly) {
     var a1 = img1[i + 3] / 255,
