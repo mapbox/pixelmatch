3b98a8e10fb693eba7e2f9dc8a34cf8a2e018a0c
Vladimir Agafonkin <agafonkin@gmail.com>
2019-06-07 10:25:18 +0300

    configurable diff output (colors, alpha)
    Co-Authored-By: VP- <vp-@users.noreply.github.com>
    Co-Authored-By: David Reignier <dreignier@users.noreply.github.com>

--- test/test.js
+++ test/test.js
@@ -6,13 +6,20 @@ const test = require('tape').test;
 const path = require('path');
 const match = require('../.');
 
-diffTest('1a', '1b', '1diff', 0.05, false, 143);
-diffTest('2a', '2b', '2diff', 0.05, false, 12437);
-diffTest('3a', '3b', '3diff', 0.05, false, 212);
-diffTest('4a', '4b', '4diff', 0.05, false, 36049);
-diffTest('5a', '5b', '5diff', 0.05, false, 0);
-diffTest('6a', '6b', '6diff', 0.05, false, 51);
-diffTest('6a', '6a', '6empty', 0, false, 0);
+const options = {threshold: 0.05};
+
+diffTest('1a', '1b', '1diff', options, 143);
+diffTest('2a', '2b', '2diff', {
+    threshold: 0.05,
+    alpha: 0.5,
+    aaColor: [0, 192, 0],
+    diffColor: [255, 0, 255]
+}, 12437);
+diffTest('3a', '3b', '3diff', options, 212);
+diffTest('4a', '4b', '4diff', options, 36049);
+diffTest('5a', '5b', '5diff', options, 0);
+diffTest('6a', '6b', '6diff', options, 51);
+diffTest('6a', '6a', '6empty', {threshold: 0}, 0);
 
 test('throws error if image sizes do not match', (t) => {
     t.throws(() => match([1, 2, 3], [1, 2, 3, 4], null, 2, 1), /Image sizes do not match/);
@@ -29,20 +36,24 @@ test('throws error if provided wrong image data format', (t) => {
     t.end();
 });
 
-function diffTest(imgPath1, imgPath2, diffPath, threshold, includeAA, expectedMismatch) {
-    const name = `comparing ${imgPath1} to ${imgPath2}, threshold: ${threshold}, includeAA: ${includeAA}`;
+function diffTest(imgPath1, imgPath2, diffPath, options, expectedMismatch) {
+    const name = `comparing ${imgPath1} to ${imgPath2}, ${JSON.stringify(options)}`;
 
     test(name, (t) => {
         const img1 = readImage(imgPath1);
         const img2 = readImage(imgPath2);
         const {width, height} = img1;
-        const expectedDiff = readImage(diffPath);
         const diff = new PNG({width, height});
 
-        const mismatch = match(img1.data, img2.data, diff.data, width, height, {threshold, includeAA});
-        const mismatch2 = match(img1.data, img2.data, null, width, height, {threshold, includeAA});
+        const mismatch = match(img1.data, img2.data, diff.data, width, height, options);
+        const mismatch2 = match(img1.data, img2.data, null, width, height, options);
 
-        t.same(diff.data, expectedDiff.data, 'diff image');
+        if (process.env.UPDATE) {
+            writeImage(diffPath, diff);
+        } else {
+            const expectedDiff = readImage(diffPath);
+            t.same(diff.data, expectedDiff.data, 'diff image');
+        }
         t.same(mismatch, expectedMismatch, 'number of mismatched pixels');
         t.same(mismatch, mismatch2, 'number of mismatched pixels without diff');
 
@@ -52,4 +63,7 @@ function diffTest(imgPath1, imgPath2, diffPath, threshold, includeAA, expectedMi
 
 function readImage(name) {
     return PNG.sync.read(fs.readFileSync(path.join(__dirname, `fixtures/${name}.png`)));
+}
+function writeImage(name, image) {
+    fs.writeFileSync(path.join(__dirname, `fixtures/${name}.png`), PNG.sync.write(image));
 }
