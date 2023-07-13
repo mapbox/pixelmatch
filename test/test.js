c9727f59d59c6b64d09183280859a7e8df5323ce
Juga Paazmaya <paazmaya@yahoo.com>
2023-07-13 12:04:18 +0300

    How about using test and assert natively from Node.js?

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const assert = require('node:assert/strict');
+const test = require('node:test');
+const fs = require('node:fs');
+const path = require('node:path');
+
 const PNG = require('pngjs').PNG;
-const fs = require('fs');
-const test = require('tape').test;
-const path = require('path');
 const match = require('../.');
 
 const options = {threshold: 0.05};
@@ -24,30 +26,27 @@ diffTest('6a', '6b', '6diff', options, 51);
 diffTest('6a', '6a', '6empty', {threshold: 0}, 0);
 diffTest('7a', '7b', '7diff', {diffColorAlt: [0, 255, 0]}, 2448);
 
-test('throws error if image sizes do not match', (t) => {
-    t.throws(() => match(new Uint8Array(8), new Uint8Array(9), null, 2, 1), 'Image sizes do not match');
-    t.end();
+test('throws error if image sizes do not match', () => {
+    assert.throws(() => match(new Uint8Array(8), new Uint8Array(9), null, 2, 1), 'Image sizes do not match');
 });
 
-test('throws error if image sizes do not match width and height', (t) => {
-    t.throws(() => match(new Uint8Array(9), new Uint8Array(9), null, 2, 1), 'Image data size does not match width/height');
-    t.end();
+test('throws error if image sizes do not match width and height', () => {
+    assert.throws(() => match(new Uint8Array(9), new Uint8Array(9), null, 2, 1), 'Image data size does not match width/height');
 });
 
-test('throws error if provided wrong image data format', (t) => {
+test('throws error if provided wrong image data format', () => {
     const err = 'Image data: Uint8Array, Uint8ClampedArray or Buffer expected';
     const arr = new Uint8Array(4 * 20 * 20);
     const bad = new Array(arr.length).fill(0);
-    t.throws(() => match(bad, arr, null, 20, 20), err);
-    t.throws(() => match(arr, bad, null, 20, 20), err);
-    t.throws(() => match(arr, arr, bad, 20, 20), err);
-    t.end();
+    assert.throws(() => match(bad, arr, null, 20, 20), err);
+    assert.throws(() => match(arr, bad, null, 20, 20), err);
+    assert.throws(() => match(arr, arr, bad, 20, 20), err);
 });
 
 function diffTest(imgPath1, imgPath2, diffPath, options, expectedMismatch) {
     const name = `comparing ${imgPath1} to ${imgPath2}, ${JSON.stringify(options)}`;
 
-    test(name, (t) => {
+    test(name, () => {
         const img1 = readImage(imgPath1);
         const img2 = readImage(imgPath2);
         const {width, height} = img1;
@@ -60,12 +59,10 @@ function diffTest(imgPath1, imgPath2, diffPath, options, expectedMismatch) {
             writeImage(diffPath, diff);
         } else {
             const expectedDiff = readImage(diffPath);
-            t.ok(diff.data.equals(expectedDiff.data), 'diff image');
+            assert.ok(diff.data.equals(expectedDiff.data), 'diff image');
         }
-        t.equal(mismatch, expectedMismatch, 'number of mismatched pixels');
-        t.equal(mismatch, mismatch2, 'number of mismatched pixels without diff');
-
-        t.end();
+        assert.equal(mismatch, expectedMismatch, 'number of mismatched pixels');
+        assert.equal(mismatch, mismatch2, 'number of mismatched pixels without diff');
     });
 }
 
